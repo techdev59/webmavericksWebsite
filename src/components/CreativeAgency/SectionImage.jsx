554dc76db0baa50d315bd8cf3b9fed626e3c39be
madhuri-dizayn <madhuri@dizayn.io>
2024-05-15 21:12:09 +0530

    In

--- src/components/CreativeAgency/SectionImage.jsx
+++ src/components/CreativeAgency/SectionImage.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import parallaxie from '@/common/parallaxie';
 import React, { useEffect } from 'react';
 
@@ -12,19 +12,5 @@ function SectionImage() {
   )
 }
 
-=======
-import parallaxie from '@/common/parallaxie';
-import React, { useEffect } from 'react';
 
-function SectionImage() {
-  useEffect(() => {
-    parallaxie('.back-image.parallaxie', 0.35);
-  }, []);
-
-  return (
-    <div className="back-image bg-img parallaxie" data-background="/dark/assets/imgs/background/17.jpg" data-overlay-dark="5" />
-  )
-}
-
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
 export default SectionImage
