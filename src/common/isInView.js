ed82cce962aa4913b7999d3316d6b8569905fdc6
madhuri-dizayn <madhuri@dizayn.io>
2024-05-15 21:04:10 +0530

    In

--- src/common/isInView.js
+++ src/common/isInView.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export default function isInView(options) {
   if (!options.selector || !options.callback) return;
 
@@ -19,26 +19,5 @@ export default function isInView(options) {
       }
     }).observe(document.querySelector(options.selector));
   }
-=======
-export default function isInView(options) {
-  if (!options.selector || !options.callback) return;
 
-  if (options.isElements) {
-    document.querySelectorAll(options.selector).forEach(element => {
-      new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) options.callback(entries[0].target);
-        else {
-          if (options.whenOutOfView) options.whenOutOfView(entries[0].target)
-        }
-      }).observe(element);
-    })
-  } else {
-    new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) options.callback(entries[0].target);
-      else {
-        if (options.whenOutOfView) options.whenOutOfView(entries[0].target)
-      }
-    }).observe(document.querySelector(options.selector));
-  }
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
 }
