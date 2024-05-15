22edbc028556a7d8b3351e46fc891a336d53e5f8
madhuri-dizayn <madhuri@dizayn.io>
2024-05-15 20:56:00 +0530

    Intial Changes

--- src/pages/dark/home-corporate/index.jsx
+++ src/pages/dark/home-corporate/index.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useEffect } from 'react';
 //= Packages
 import Head from 'next/head';
@@ -54,61 +54,5 @@ function HomeCorporate() {
 
 HomeCorporate.getLayout = page => <Layout>{page}</Layout>
 
-=======
-import React, { useEffect } from 'react';
-//= Packages
-import Head from 'next/head';
-//= Layout
-import Layout from '@/layouts/default';
-//= Components
-import Loader from '@/components/Common/Loader';
-import LinesTwo from '@/components/Common/LinesTwo';
-import Navbar from '@/components/Common/MainNavbar';
-import Header from '@/components/Corporate/Header';
-import Services from '@/components/Corporate/Services';
-import About from '@/components/Corporate/About';
-import Marq from '@/components/Corporate/Marq';
-import Portfolio from '@/components/Corporate/Portfolio';
-import Numbers from '@/components/Corporate/Numbers';
-import Testimonials from '@/components/Corporate/Testimonials';
-import Block from '@/components/Corporate/Block';
-import Pricing from '@/components/Corporate/Pricing';
-import Blog from '@/components/Corporate/Blog';
-import Footer from '@/components/Corporate/Footer';
 
-function HomeCorporate() {
-  useEffect(() => {
-    document.body.classList.add('home-corp');
-    return () => document.body.classList.remove('home-corp');
-  }, []);
-
-  return (
-    <>
-      <Head>
-        <title>Geekfolio - Corporate</title>
-      </Head>
-
-      <Loader />
-      <LinesTwo />
-      <Navbar />
-      <main className="main-bg">
-        <Header />
-        <Services />
-        <About />
-        <Marq />
-        <Portfolio />
-        <Numbers />
-        <Testimonials />
-        <Block />
-        <Pricing />
-        <Blog />
-      </main>
-      <Footer />
-    </>
-  )
-}
-
-HomeCorporate.getLayout = page => <Layout>{page}</Layout>
-
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
 export default HomeCorporate;
--- src/pages/dark/home-digital-agency/index.jsx
+++ src/pages/dark/home-digital-agency/index.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useEffect } from 'react';
 //= Packages
 import Head from 'next/head';
@@ -56,63 +56,5 @@ function HomeDigitalAgency() {
 
 HomeDigitalAgency.getLayout = page => <Layout>{page}</Layout>
 
-=======
-import React, { useEffect } from 'react';
-//= Packages
-import Head from 'next/head';
-//= Layout
-import Layout from '@/layouts/default';
-//= Components
-import Loader from '@/components/Common/Loader';
-import LinesTwo from '@/components/Common/LinesTwo';
-import Navbar from '@/components/Common/MainNavbar';
-import Header from '@/components/DigitalAgency/Header';
-import Services from '@/components/DigitalAgency/Services';
-import Block from '@/components/DigitalAgency/Block';
-import Portfolio from '@/components/DigitalAgency/Portfolio';
-import Price from '@/components/DigitalAgency/Price';
-import ServicesTab from '@/components/DigitalAgency/ServicesTab';
-import Clients from '@/components/DigitalAgency/Clients';
-import Testimonials from '@/components/DigitalAgency/Testimonials';
-import Approach from '@/components/DigitalAgency/Approach';
-import CallToAction from '@/components/DigitalAgency/CallToAction';
-import Blog from '@/components/DigitalAgency/Blog';
-import Footer from '@/components/DigitalAgency/Footer';
 
-function HomeDigitalAgency() {
-  useEffect(() => {
-    document.body.classList.add('home-digital', 'sub-bg');
-    return () => document.body.classList.remove('home-digital', 'sub-bg');
-  }, []);
-
-  return (
-    <>
-      <Head>
-        <title>Geekfolio - Digital Agency</title>
-      </Head>
-
-      <Loader />
-      <LinesTwo />
-      <Navbar mainBg />
-      <Header />
-      <main className="position-re">
-        <Services />
-        <Block />
-        <Portfolio />
-        <Price />
-        <ServicesTab />
-        <Clients />
-        <Testimonials />
-        <Approach />
-        <CallToAction />
-        <Blog />
-      </main>
-      <Footer />
-    </>
-  )
-}
-
-HomeDigitalAgency.getLayout = page => <Layout>{page}</Layout>
-
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
 export default HomeDigitalAgency;
