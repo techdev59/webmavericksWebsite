b30db0577f17e822539f184620232591a323ad2d
madhuri-dizayn <madhuri@dizayn.io>
2024-05-15 20:04:42 +0530

    All Changes Done

--- src/components/InnerPages/ProjectDetails2/About.jsx
+++ src/components/InnerPages/ProjectDetails2/About.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useEffect } from 'react';
 //= Scripts
 import parallaxie from '@/common/parallaxie';
@@ -39,46 +39,4 @@ function About() {
 	)
 }
 
-=======
-import React, { useEffect } from 'react';
-//= Scripts
-import parallaxie from '@/common/parallaxie';
-
-function About() {
-	useEffect(() => {
-		parallaxie('.back-image.parallaxie', 0.4);
-	}, []);
-
-	return (
-		<section className="section-padding">
-			<div className="container">
-				<div className="row justify-content-center">
-					<div className="col-lg-8">
-						<div className="cont">
-							<h3 className="mb-15">About the Project.</h3>
-							<p>Won’t seasons, appear days them stars replenish divided. All second forth. Him place was seas man and gathering creepeth called fly. Them sea place lights, midst bearing fourth above.</p>
-							<div className="info d-flex mt-50">
-								<div className="item mr-50">
-									<h6 className="fz-16">Category</h6>
-									<span className="sub-title ls1">Digital Design</span>
-								</div>
-								<div className="item mr-50">
-									<h6 className="fz-16">Customer</h6>
-									<span className="sub-title ls1">ThemesCamp</span>
-								</div>
-								<div className="item">
-									<h6 className="fz-16">Date</h6>
-									<span className="sub-title ls1">August 6, 2020</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="back-image bg-img parallaxie mt-100" data-background="/dark/assets/imgs/works/projects/2/7.png"></div>
-		</section>
-	)
-}
-
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
 export default About
--- src/components/Preview/Demos.jsx
+++ src/components/Preview/Demos.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import Link from 'next/link';
 //= Data
@@ -41,48 +41,4 @@ function Demos() {
   )
 }
 
-=======
-import React from 'react';
-import Link from 'next/link';
-//= Data
-import data from '@/data/Preview/demos.json';
-
-function Demos() {
-  return (
-    <section className="demos section-padding pt-80">
-      <div className="container-xxl">
-        <div className="row">
-          {
-            data.map(item => (
-              <div className="col-lg-4 col-md-6" key={item.id}>
-                <div className="item text-center">
-                  <Link href={item.link} target="_blank">
-                    <div className="img">
-                      <img src={item.image} alt="" />
-                    </div>
-                    <h6 className="mt-15">{item.title} {item.mode === 'light' && <span>(Light)</span>}</h6>
-                  </Link>
-                  {
-                    item.new &&
-                    <span className="new">New</span>
-                  }
-                </div>
-              </div>
-            ))
-          }
-          <div className="col-lg-4 col-md-6">
-            <div className="item text-center">
-              <div className="img">
-                <img src="/landing-preview/img/demos/more.jpg" alt="" />
-              </div>
-              <h6 className="mt-15">Coming More</h6>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
-
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
 export default Demos
