0687da434447a0cf5b1c7fb642df9f196e1915c1
madhuri-dizayn <madhuri@dizayn.io>
2024-05-15 18:47:06 +0530

    All Changes1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
@@ -17,23 +17,4 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-=======
-/** @type {import('next').NextConfig} */
-const path = require("path");
 
-const nextConfig = {
-  reactStrictMode: false,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "css")],
-  },
-  trailingSlash: true,
-  devIndicators: {
-    buildActivity: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  }
-}
-
-module.exports = nextConfig
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
