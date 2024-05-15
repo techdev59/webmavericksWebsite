afa41a300afea1b761c0a0c49b228fb87619c9eb
madhuri-dizayn <madhuri@dizayn.io>
2024-05-15 20:58:35 +0530

    Intial Changes1

--- src/components/Common/LinesTwo.jsx
+++ src/components/Common/LinesTwo.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 
 function LinesTwo() {
@@ -7,14 +7,5 @@ function LinesTwo() {
   )
 }
 
-=======
-import React from 'react';
 
-function LinesTwo() {
-  return (
-    <div className="lines two-out"></div>
-  )
-}
-
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
 export default LinesTwo
--- src/components/Corporate/Block/index.jsx
+++ src/components/Corporate/Block/index.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 //= Components
 import Values from './Values';
@@ -15,22 +15,5 @@ function Block({ lightMode }) {
   )
 }
 
-=======
-import React from 'react';
-//= Components
-import Values from './Values';
-import Video from './Video';
 
-function Block({ lightMode }) {
-  return (
-    <section className="block-sec section-padding">
-      <div className="container">
-        <Video lightMode={lightMode} />
-        <Values />
-      </div>
-    </section>
-  )
-}
-
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
 export default Block
