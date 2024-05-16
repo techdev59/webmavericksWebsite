0711b2cfa601bca73d287e0d0efaaf19ae1867a1
madhuri-dizayn <madhuri@dizayn.io>
2024-05-16 10:22:27 +0530

    changes

--- src/common/marketing-agency-customjs.js
+++ src/common/marketing-agency-customjs.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export default function customScript() {
   document.documentElement.className = "js";
   var supportsCssVars = function () {
@@ -9,16 +9,5 @@ export default function customScript() {
       t.parentNode.removeChild(t), e
   };
   supportsCssVars() || alert("Please view this demo in a modern browser that supports CSS Variables.");
-=======
-export default function customScript() {
-  document.documentElement.className = "js";
-  var supportsCssVars = function () {
-    var e, t = document.createElement("style");
-    return t.innerHTML = "root: { --tmp-var: bold; }",
-      document.head.appendChild(t),
-      e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")),
-      t.parentNode.removeChild(t), e
-  };
-  supportsCssVars() || alert("Please view this demo in a modern browser that supports CSS Variables.");
->>>>>>> 2a8764e07c23f76597450f9070008c504f861aa7
+
 }
